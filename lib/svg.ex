d26e444f251397659d144d29f41d77bda3efa644
Jeff Weiss <jweiss@adobe.com>
2026-01-03 14:55:26 -0800

    initial repo creation

---
+++ lib/svg.ex
@@ -0,0 +1,18 @@
+defmodule Svg do
+  @moduledoc """
+  Documentation for `Svg`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Svg.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/svg_test.exs
@@ -0,0 +1,8 @@
+defmodule SvgTest do
+  use ExUnit.Case
+  doctest Svg
+
+  test "greets the world" do
+    assert Svg.hello() == :world
+  end
+end
